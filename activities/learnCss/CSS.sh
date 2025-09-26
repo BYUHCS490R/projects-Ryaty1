07fdcb49d4b3eb108514c061aa14b93a04ae6a6f
Ryaty1 <hengkimchhay1@gmail.com>
2025-09-26 12:38:29 -1000

    done

---
+++ activities/learnCss/CSS.sh
@@ -0,0 +1,8 @@
+
+#CSS PRIORITY:
+1- HTML Attributes
+2- Inline CSS: like style="color:blue;" #DON'T USE IT, YOU'LL LOSE POINTS IN THIS CLASS.
+3- Embedded CSS #DON'T USE IT, YOU'LL LOSE POINTS IN THIS CLASS.
+4- External CSS #USE IT, YOU'LL GET POINTS IN THIS CLASS.
+
+#CSS., put link to external CSS file in the <head> section of your HTML file.
